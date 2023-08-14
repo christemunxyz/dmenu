85229a81a1a1f768d7a4d6e2a34b88a30719f26f
goku <126220374+christemunxyz@users.noreply.github.com>
2023-08-14 15:56:31 -0400

    Update config.h

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#121314" },
-	[SchemeSel] = { "#eeeeee", "#df6b74" },
+	[SchemeSel] = { "#eeeeee", "#a4540d" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
